8a4fcabb4fc8b479b37188721f91654e61954392
XTU-Liuyun <1824031590@qq.com>
2023-07-24 01:26:13 +0800

    创建区域

--- MyBlog.Web/Program.cs
+++ MyBlog.Web/Program.cs
@@ -17,6 +17,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "Admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
